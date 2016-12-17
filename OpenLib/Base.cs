fbb9fd2ffccc5c5805622a0f887eb372a7938b58
greatbody <wedown@163.com>
2016-12-17 15:48:53 +0800

    init

---
+++ OpenLib/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenLib
+{
+    public class Base
+    {
+        static string Version = "1.0.0";
+    }
+}
